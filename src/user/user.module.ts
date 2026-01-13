f600e88a5f740c5f033052021cb90f371c85bb20
s0fy4n002 <s0fy4n002@gmail.com>
2026-01-13 14:31:46 +0700

    init middleware User

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UserController } from './user/user.controller';
+import { UserMiddleware } from './middleware/user.middleware';
 
 @Module({
   controllers: [UserController]
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(UserMiddleware)
+      .forRoutes('api/users');
+  }
+}
