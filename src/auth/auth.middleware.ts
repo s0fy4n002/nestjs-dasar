38dd77650bfb8b17f66308c82f683f927ee99369
s0fy4n002 <s0fy4n002@gmail.com>
2026-01-30 01:39:34 +0700

    auth middleware

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -1,10 +1,25 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { HttpException, Injectable, NestMiddleware } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma/prisma.service';
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
   constructor(private readonly prismaService: PrismaService){}
   use(req: any, res: any, next: () => void) {
+    const username = Number(req.headers['x-username']);
+    if(!username){
+      throw new HttpException('Unauthorized', 401)
+    }
+    const user = this.prismaService.users.findUnique({
+      where:{
+        id: username
+      }
+    })
+
+    if(!user){
+      throw new HttpException('Unauthorized', 401)
+    }
+    
+    req.user = user
     next();
   }
 }
